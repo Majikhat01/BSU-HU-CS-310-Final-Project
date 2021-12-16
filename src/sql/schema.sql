c28bfbbac0f4b5d57326ae735ab7b5677cafb765
Calvin Hatfield <70164922+Majikhat01@users.noreply.github.com>
2021-12-16 14:17:23 -0700

    Update schema.sql

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -31,3 +31,40 @@ CREATE TABLE IF NOT EXISTS students(
     birthdate DATE, 
     PRIMARY KEY (student_id) 
 );
+
+CREATE TABLE IF NOT EXISTS academic_titles(
+    academic_title_id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    PRIMARY KEY(academic_title_id)
+ );
+  
+CREATE TABLE IF NOT EXISTS instructors(
+    instructor_id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(80) NOT NULL,
+    last_name VARCHAR(80) NOT NULL,
+    academic_title_id INT,
+    FOREIGN KEY(academic_title_id) REFERENCES academic_titles(academic_title_id),
+    PRIMARY KEY(instructor_id)
+);
+
+CREATE TABLE IF NOT EXISTS terms(
+    term_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(80) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS class_sections(
+    class_section_id INT NOT NULL AUTO_INCREMENT,
+    class_id INT NOT NULL,
+    instructor_id INT NOT NULL,
+    term_id NOT NULL,
+    PRIMARY KEY(class_section_id),
+    FOREIGN KEY(class_id) REFERENCES classes(class_id),
+    FOREIGN KEY(instructor_id) REFERENCES instructors(instructor_id),
+    FOREIGN KEY(term_id) REFERENCES terms(term_id)
+);
+
+CREATE TABLE IF NOT EXISTS grades(
+    grade_id INT NOT NULL AUTO_INCREMENT,
+    letter_grade CHAR(2) NOT NULL,
+    PRIMARY KEY(grade_id)
+);
